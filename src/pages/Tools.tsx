6357fdebb7e485a6076d3085509611f1182f16af
Victor Ogunwehin <victorogunwehin@gmail.com>
2026-03-06 15:35:06 +0100

    updates_tools

--- src/pages/Tools.tsx
+++ src/pages/Tools.tsx
@@ -31,7 +31,7 @@ const Tools: React.FC = () => {
     setTimeout(() => setSpinning(false), 3000);
   };
 
-  const gtourSteps = [
+  const tourSteps = [
     {
       title: 'Welcome to Tools',
       description: 'This page demonstrates various utility components',
